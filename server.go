60688089ee03b7efcf113d4b5b348293ef12ce59
xeonx <simon.hege@gmail.com>
2017-01-04 15:17:25 +0100

    Use openid connect for authentication

--- server.go
+++ server.go
@@ -8,11 +8,9 @@ import (
 	"context"
 	"encoding/json"
 	"fmt" //TODO: rearchitecture so that we can log easily
-	"io/ioutil"
 	"net/http"
-	"time"
 
-	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/coreos/go-oidc"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/pkg/errors"
@@ -20,15 +18,13 @@ import (
 
 // Config provides the configuration for the API server
 type Config struct {
-	Addr string //The address to listen on
-
-	SigningKey      []byte
-	SigningDuration time.Duration
+	Addr                string //The address to listen on
+	OpenIDConnectIssuer string //The issuer of authorisation tokens
 }
 
-type Chain []func(next http.Handler) http.Handler
+type chain []func(next http.Handler) http.Handler
 
-func (c Chain) Handler(h http.Handler) http.Handler {
+func (c chain) Handler(h http.Handler) http.Handler {
 	for i := len(c) - 1; i >= 0; i-- {
 		h = c[i](h)
 	}
@@ -37,48 +33,28 @@ func (c Chain) Handler(h http.Handler) http.Handler {
 
 // Server contains instance details for the server
 type Server struct {
-	CheckPassword      func(ctx context.Context, userID, password string) error
 	cfg                Config
 	mux                *mux.Router
-	middlewares        Chain
-	privateMiddlewares Chain
+	middlewares        chain
+	privateMiddlewares chain
 }
 
 // New returns a new instance of the server based on the specified configuration.
-func New(cfg Config) *Server {
+func New(cfg Config) (*Server, error) {
 	s := Server{
-		CheckPassword: func(ctx context.Context, userID, password string) error {
-			return errors.New("not implemented")
-		},
 		cfg: cfg,
 		mux: mux.NewRouter(),
 	}
 
 	//TODO: add global middlewares, based on cfg
-
-	s.privateMiddlewares = append(s.privateMiddlewares, s.AuthenticatedFilter())
-
-	return &s
-}
-
-//Login sets the path to authenticate
-func (s *Server) Login(loginPath string) { //TODO add refreshPath or switch to dex
-	s.Public(NewJSONRoute("GET", loginPath, s.loginHandler))
-	s.Public(NewJSONRoute("POST", loginPath, s.loginHandler))
-}
-
-func (s *Server) loginHandler(req *http.Request) (interface{}, error) {
-
-	token, err := s.Authenticate(req)
+	authFilter, err := s.AuthenticatedFilter()
 	if err != nil {
-		return nil, errors.Wrap(err, "Authenticate failed")
+		return nil, err
 	}
 
-	return struct {
-		Token string `json:"token"`
-	}{
-		Token: token,
-	}, nil
+	s.privateMiddlewares = append(s.privateMiddlewares, authFilter)
+
+	return &s, nil
 }
 
 //Public adds a publicly available route to the server
@@ -93,15 +69,21 @@ func (s *Server) Private(route Route) {
 
 // addRouterWithChain adds one or multiple routers for the server.New
 //The given middleware chain will be added to each route.
-func (s *Server) addRouteWithChain(chain Chain, r Route) {
+func (s *Server) addRouteWithChain(chain chain, r Route) {
 
-	s.mux.Handle(r.Path(), chain.Handler(r.Handler())).Methods(r.Method())
-
+	if r.IsPrefix() {
+		s.mux.PathPrefix(r.Path()).Handler(chain.Handler(r.Handler())).Methods(r.Method())
+	} else {
+		s.mux.Handle(r.Path(), chain.Handler(r.Handler())).Methods(r.Method())
+	}
 }
 
 //Handler returns the handler provided by the server
 func (s *Server) Handler() http.Handler {
-	return handlers.CORS(handlers.AllowedHeaders([]string{"Authorization", "Content-Type"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"}))(s.mux)
+	return handlers.CORS(
+		handlers.AllowedHeaders([]string{"Authorization", "Content-Type"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"}),
+	)(s.mux)
 }
 
 //Run is a convenience function that runs the server as an HTTP server.
@@ -112,45 +94,9 @@ func (s *Server) Run() error {
 //ErrInvalidToken is raised when the token is not valid
 var ErrInvalidToken = errors.New("Invalid token. Please try to reconnect.")
 
-//ErrExpiredToken is raised when the token has expired
-var ErrExpiredToken = errors.New("Expired token. Please try to reconnect.")
-
 //Param extract an URL parameter
 func Param(r *http.Request, name string) string {
 	return mux.Vars(r)[name]
-}
-
-func (s *Server) validateToken(tokenString string) (*jwt.Token, error) {
-
-	// validate the token
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return s.cfg.SigningKey, nil
-	})
-
-	// branch out into the possible error from signing
-	switch err.(type) {
-
-	case nil: // no error
-		if !token.Valid { // but may still be invalid
-			return nil, ErrInvalidToken
-		}
-
-	case *jwt.ValidationError: // something was wrong during the validation
-		vErr := err.(*jwt.ValidationError)
-
-		switch vErr.Errors {
-		case jwt.ValidationErrorExpired:
-			return nil, ErrExpiredToken
-
-		default:
-			return nil, err
-		}
-
-	default: // something else went wrong
-		return nil, err
-	}
-
-	return token, nil
 }
 
 //Route defines an individual API route in the server.
@@ -161,6 +107,8 @@ type Route interface {
 	Method() string
 	// Path returns the subpath where the route responds to.
 	Path() string
+	// IsPrefix returns wheter the rout is for a single path (false) or a wildcard
+	IsPrefix() bool
 }
 
 func handleError(r *http.Request, w http.ResponseWriter, err error) {
@@ -236,9 +184,10 @@ func NewRouteFunc(method, path string, handler http.HandlerFunc) Route {
 }
 
 type basicRoute struct {
-	m string
-	p string
-	h http.Handler
+	m      string
+	p      string
+	h      http.Handler
+	prefix bool
 }
 
 func (r basicRoute) Handler() http.Handler {
@@ -250,13 +199,8 @@ func (r basicRoute) Method() string {
 func (r basicRoute) Path() string {
 	return r.p
 }
-
-type handlerC struct {
-	http.Handler
-}
-
-func (h handlerC) ServeHTTPC(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-	h.ServeHTTP(w, r)
+func (r basicRoute) IsPrefix() bool {
+	return r.prefix
 }
 
 //NewStaticFilesRoute creates a new route that returns static files
@@ -268,54 +212,21 @@ func NewStaticFilesRoute(pathPrefix string, root http.FileSystem) Route {
 
 	h := http.StripPrefix(pathPrefix, http.FileServer(root))
 
-	return NewRoute("GET", pathPrefix+"/*path", handlerC{h})
+	return basicRoute{
+		m:      "GET",
+		p:      pathPrefix + "/",
+		h:      h,
+		prefix: true,
+	}
 }
 
 //NewRedirectRoute creates a new route that redirect to a new URL
 func NewRedirectRoute(from, to string) Route {
-	return NewRoute("GET", from, handlerC{http.RedirectHandler(to, http.StatusMovedPermanently)})
+	return NewRoute("GET", from, http.RedirectHandler(to, http.StatusMovedPermanently))
 }
 
-//Authenticate returns a signed token if the request contains valid userID and password
-func (s *Server) Authenticate(r *http.Request) (string, error) {
-	userID := r.FormValue("userID")
-	password := r.FormValue("password")
-
-	if len(userID) == 0 {
-		if r.Body != nil {
-			if body, err := ioutil.ReadAll(r.Body); err == nil {
-				defer r.Body.Close()
-				var jsonItem struct {
-					UserID   string `json:"userID"`
-					Password string `json:"password"`
-				}
-				if err := json.Unmarshal(body, &jsonItem); err == nil {
-					userID = jsonItem.UserID
-					password = jsonItem.Password
-				}
-			}
-		}
-	}
-
-	ctx := r.Context()
-
-	err := s.CheckPassword(ctx, userID, password)
-	if err != nil {
-		return "", errors.Wrap(err, "password check failed")
-	}
-
-	// Create the token
-	token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
-		"userId": userID,
-		"exp":    time.Now().Add(s.cfg.SigningDuration).Unix(),
-	})
-
-	// Sign and get the complete encoded token as a string
-	return token.SignedString(s.cfg.SigningKey)
-}
-
-//CheckAuthorization returns the decoded valid token if any
-func (s *Server) CheckAuthorization(r *http.Request) (*jwt.Token, error) {
+//GetRawIDToken returns the raw token if any
+func GetRawIDToken(r *http.Request) (string, error) {
 
 	//Retrieve JWT from Authorization header (or auth form parameter)
 	bearerString := r.Header.Get("Authorization")
@@ -331,45 +242,67 @@ func (s *Server) CheckAuthorization(r *http.Request) (*jwt.Token, error) {
 		}
 	}
 	if len(bearerString) < len("Bearer ") {
-		return nil, ErrInvalidToken
+		return "", ErrInvalidToken
 	}
-	tokenString := bearerString[len("Bearer "):]
-
-	token, err := s.validateToken(tokenString)
-	if err != nil {
-		return nil, err
-	}
-
-	return token, nil
+	return bearerString[len("Bearer "):], nil
 }
 
 type key int
 
-const userIDKey key = 0
+const userInfoKey key = 0
 
-func newContextWithUserID(ctx context.Context, userID string) context.Context {
-	return context.WithValue(ctx, userIDKey, userID)
+func newContextWithUserInfo(ctx context.Context, u userInfo) context.Context {
+	return context.WithValue(ctx, userInfoKey, u)
 }
 
-//GetUserID returns the current ID of the logged in user, if any
-func GetUserID(ctx context.Context) (string, error) {
-	userID, ok := ctx.Value(userIDKey).(string)
+//UserInfo contains basic information for an authenticated user
+type UserInfo interface {
+	ID() string
+	DisplayName() string
+	Email() string
+}
+
+type userInfo struct {
+	subject string
+	name    string
+	email   string
+}
+
+func (u userInfo) ID() string {
+	return u.subject
+}
+func (u userInfo) DisplayName() string {
+	return u.name
+}
+func (u userInfo) Email() string {
+	return u.email
+}
+
+//GetUserInfo returns the current ID of the logged in user, if any
+func GetUserInfo(ctx context.Context) (UserInfo, error) {
+	userInfo, ok := ctx.Value(userInfoKey).(userInfo)
 	if !ok {
-		return "", errors.New("Invalid user ID")
+		return nil, errors.New("Invalid user ID")
 	}
 
-	return userID, nil
+	return userInfo, nil
 }
 
-// AuthenticatedFilter filters logged in users. Users with an invalid session are redirected to the loginURL.
-func (s *Server) AuthenticatedFilter( /*loginURL string*/ ) func(next http.Handler) http.Handler {
+// AuthenticatedFilter filters logged in users
+func (s *Server) AuthenticatedFilter() (func(next http.Handler) http.Handler, error) {
+
+	provider, err := oidc.NewProvider(context.Background(), s.cfg.OpenIDConnectIssuer)
+	if err != nil {
+		return nil, err
+	}
+	verifier := provider.Verifier(oidc.VerifyExpiry())
+
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 			ctx := r.Context()
-
-			token, err := s.CheckAuthorization(r)
-			if err == ErrInvalidToken || err == ErrExpiredToken { //TODO: add redirect
+			rawIDToken, err := GetRawIDToken(r)
+			if err == ErrInvalidToken {
 				http.Error(w, err.Error(), http.StatusUnauthorized)
 				return
 			} else if err != nil {
@@ -377,23 +310,35 @@ func (s *Server) AuthenticatedFilter( /*loginURL string*/ ) func(next http.Handl
 				return
 			}
 
-			signedString, err := token.SignedString(s.cfg.SigningKey)
+			idToken, err := verifier.Verify(ctx, rawIDToken)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusUnauthorized)
+				return
+			}
+
 			http.SetCookie(w, &http.Cookie{
 				Name:  "bearer-token",
-				Value: signedString,
+				Value: rawIDToken,
 				Path:  "/pages/",
 			})
 
-			//Store user in new context
-			claims := token.Claims.(jwt.MapClaims)
-			userID, ok := claims["userId"].(string)
-			if !ok {
-				http.Error(w, "Invalid user ID in token", http.StatusInternalServerError)
+			var claims struct {
+				Email string `json:"email"`
+				Name  string `json:"name"`
+			}
+			if err := idToken.Claims(&claims); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
-			ctx = newContextWithUserID(ctx, userID)
+
+			//Store user in new context
+			ctx = newContextWithUserInfo(ctx, userInfo{
+				subject: idToken.Subject,
+				name:    claims.Name,
+				email:   claims.Email,
+			})
 
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
-	}
+	}, nil
 }
